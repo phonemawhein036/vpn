15792a23e9b19d530a979c3fd74f4fb767af1788
phonemawhein036 <phonemawhein036@googlemail.com>
2026-03-29 16:56:48 +0630

    Update generate.js

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -69,7 +69,7 @@ MTU = 1280
 PublicKey = bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=
 AllowedIPs = 0.0.0.0/0
 AllowedIPs = ::/0
-Endpoint = 162.159.192.3:500
+Endpoint = 162.159.192.5:500
 PersistentKeepalive = 20`;
     }
 
