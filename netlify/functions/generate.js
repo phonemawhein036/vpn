ff47aacb5457772140aad85923d62f3118ede385
phonemawhein036 <phonemawhein036@googlemail.com>
2026-03-29 13:39:37 +0630

    Update generate.js

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -54,7 +54,7 @@ MTU = 1280
 PublicKey = ${peerPub}
 AllowedIPs = 0.0.0.0/0
 AllowedIPs = ::/0
-Endpoint = 162.159.192.3:500
+Endpoint = 162.159.192.5:500
 PersistentKeepalive = 20`;
     } else {
       configStr =
